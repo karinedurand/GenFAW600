a941bc7aba8cfc421fef6fa1754d010e0aa5b991
karine Durand <45596232+karinedurand@users.noreply.github.com>
2025-08-26 08:13:06 +0200

    Update 05.extract_and _rename.sh

--- VCF_Filtering_and_Renaming/05.extract_and _rename.sh
+++ VCF_Filtering_and_Renaming/05.extract_and _rename.sh
@@ -2,7 +2,7 @@
 #SBATCH -p dgimi-eha
 
 
-VCF_IN="/lustre/durandk/BIG_DATABASE/FULL_BIGDATA/out.vcf.gz"
+VCF_IN="/lustre/durandk/BIG_DATABASE/FULL_BIGDATA/ALLsnp.noindel.vcf.gz"
 VCF_OUT="//lustre/durandk/GenFAW600/PCA/GenFAW600_rename.vcf"
 
 
@@ -44,8 +44,13 @@ EOL
 # 🟢 Extraction des 29 scaffolds avec vcftools et renommage CHROM
 /storage/simple/projects/faw_adaptation/programs/vcftools_0.1.13/bin/vcftools \
     --gzvcf "$VCF_IN" \
-    $(awk '{print "--chr "$1}' "$CHROM_LIST") \
-    --recode --stdout | sed 's/HiC_scaffold_//' > "$VCF_OUT"
+    $(awk '{print "--chr " $1}' "${CHROM_LIST}") \
+    --remove remove_samples.txt \
+   --recode --stdout > GenFAW600.vcf
+    
+    
+/storage/simple/projects/faw_adaptation/programs/htslib-1.9/bgzip -c GenFAW600.vcf  >>  GenFAW600.vcf.gz
+zcat GenFAW600.vcf.gz | sed 's/HiC_scaffold_//'  >> GenFAW600.rename.vcf
 
 
 # 🟢 Compression et indexation du fichier VCF
