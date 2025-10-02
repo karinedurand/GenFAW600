cc3d654cdaa20e89d536ef37b9dee23b5e52a395
karine Durand <45596232+karinedurand@users.noreply.github.com>
2025-10-02 09:38:53 +0200

    Update PRE_traitement_sNMF.sh

--- sNMF/PRE_traitement_sNMF.sh
+++ sNMF/PRE_traitement_sNMF.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+#SBATCH -p cpu-ondemand
+#SBATCH --mem=120G
 
+
+module load bioinfo-cirad
+module load java/jre1.8.0_31
+
+cd /lustre/durandk/GenFAW600/sNMF/
+
+source /home/durandk/miniconda3/etc/profile.d/conda.sh
+conda activate plink2
+plink2 --bfile GenFAW600_Whole_569_Pruned \
+ 	--chr-set 29 \
+ 	--allow-extra-chr \
+       --geno 0.8 \
+       --recode vcf  \
+       --out GenFAW600_geno0.8
