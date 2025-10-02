1cab9d2e069b8d6df398577b32f09253660fea51
karine Durand <45596232+karinedurand@users.noreply.github.com>
2025-10-02 09:40:45 +0200

    Update lancer_ped2geno_R.sh

--- sNMF/lancer_ped2geno_R.sh
+++ sNMF/lancer_ped2geno_R.sh
@@ -3,22 +3,8 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 
- module load     bioinfo/PLINK/1.90b7 
-
-
-#!/bin/bash
 
 # Filtrer individus avec plus de 80% de génotypes manquants, SNPs (>5% missing), et convertir en VCF en une commande
-
-
-# plink --bfile GenFAW600_Whole_569_Pruned \
-# 	--chr-set 29 \
-# 	--allow-extra-chr \
-#       --geno 0.8 \
-#       --recode vcf  \
-#       --out GenFAW600_geno0.8
-
-
 
 module load statistics/R/4.3.0
 
