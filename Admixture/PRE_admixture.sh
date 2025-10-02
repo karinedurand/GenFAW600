dfed34e60042593137aad277e1f9af91010b9c87
karine Durand <45596232+karinedurand@users.noreply.github.com>
2025-10-02 09:33:06 +0200

    Update PRE_admixture.sh

--- Admixture/PRE_admixture.sh
+++ Admixture/PRE_admixture.sh
@@ -1,1 +1,30 @@
+#SBATCH -p cpu-dedicated
+#SBATCH --account=dedicated-cpu@dgimi-eha
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=8
 
+
+conda activate plink2
+
+
+
+plink2 --bfile GenFAW600_Whole_569 \
+  --chr-set 29 \
+  --allow-extra-chr \
+       --geno 1 \
+       --make-bed \
+       --out GenFAW600_Whole_569_noAllMissing
+  
+
+plink2 --bfile GenFAW600_Whole_569_noAllMissing \
+	  --chr-set 29 \
+	  --allow-extra-chr \
+	  --indep-pairwise 50 5 0.2 \
+	  --out GenFAW600_Whole_569_noAllMissing_LDprune
+
+plink2 --bfile GenFAW600_Whole_569_noAllMissing \
+	  --chr-set 29 \
+	  --allow-extra-chr \
+	  --extract GenFAW600_Whole_569_noAllMissing_LDprune.prune.in \
+	  --make-bed \
+	  --out GenFAW600_Whole_569_noAllMissing_Pruned
