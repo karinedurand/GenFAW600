7993e93a845f784e5ab08dc13aac7c8856379fde
karine Durand <45596232+karinedurand@users.noreply.github.com>
2025-10-13 16:19:02 +0200

    Update and rename 06.2.Whole_genome_filtering.sh to 10.Whole_genome_filtering.sh

---
+++ VCF_Filtering_and_Renaming/10.Whole_genome_filtering.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#SBATCH -p cpu-dedicated
+#SBATCH --account=dedicated-cpu@dgimi-eha
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=8
+
+set -euo pipefail
+# ==============================
+# ENVIRONNEMENTS
+# ==============================
+source /home/durandk/miniconda3/etc/profile.d/conda.sh
+conda activate bgzip_tabix
+#mv /storage/simple/users/durandk/scratch_durandk/GenFAW600/VCF/VCF202510_new/GenFAW600_Whole_nocontaminated.vcf.gz.recode.vcf /storage/simple/users/durandk/scratch_durandk/#GenFAW600/VCF/VCF_filtering/whole_genome/
+
+cd /storage/simple/users/durandk/scratch_durandk/GenFAW600/VCF/VCF202510_new/whole_genome
+
+#bgzip -c GenFAW600_Whole_nocontaminated.vcf.gz.recode.vcf  > GenFAW600_Whole_nocontaminated.vcf.gz.recode.vcf.gz
+#tabix -p vcf GenFAW600_Whole_nocontaminated.vcf.gz.recode.vcf.gz
+# ==============================
+# Conversion en PLINK
+# ==============================
+conda activate plink2
+
+#plink2 --vcf GenFAW600_Whole_nocontaminated.vcf.gz.recode.vcf.gz \
+#  --make-bed \
+#  --min-alleles 2 --max-alleles 2 \
+#  --set-all-var-ids '@:#:$r:$a' \
+#  --chr-set 29 \
+#  --allow-extra-chr \
+#  --out GenFAW600_Whole_biallelic
+
+
+#plink2 --bfile GenFAW600_Whole \
+#       --chr-set 29 \
+#       --allow-extra-chr \
+#       --missing \
+#        -out GenFAW600_Whole_missing
+
+#plink2 --bfile GenFAW600_Whole_biallelic \
+#	  --chr-set 29 \
+#	  --allow-extra-chr \
+#	  --geno 0.2 \
+#	  --make-bed \
+#	  --out GenFAW600_Whole_biallelic_geno0.2
+
+
+#plink2 --bfile GenFAW600_Whole_biallelic_geno0.2 \
+#	  --chr-set 29 \
+#	  --allow-extra-chr \
+#	  --indep-pairwise 50 5 0.5 \
+#	  --out GenFAW600_Whole_biallelic_geno0.2
+
+#plink2 --bfile GenFAW600_Whole_biallelic_geno0.2 \
+#	  --chr-set 29 \
+#	  --allow-extra-chr \
+#	  --extract GenFAW600_Whole_biallelic_geno0.2.prune.in \
+#	  --make-bed \
+#	  --out GenFAW600_Whole_biallelic_geno0.2_Pruned0.5
+
+# ==============================
+# PCA #--geno 0.2: eliminates SNPs with more than 20% missing, retaining those with at least 80% of data present.
+# ==============================
+plink2 --bfile GenFAW600_Whole_biallelic_geno0.2_Pruned0.5 \
+	   --chr-set 29 \
+	  --allow-extra-chr \
+	  --pca \
+	  --out GenFAW600_Whole_biallelic_geno0.2_Pruned0.5_PCA
+
+
+
+
+
