5b29f05dcf56958dfe5314dbca1dcf146d269594
karine Durand <45596232+karinedurand@users.noreply.github.com>
2025-11-14 20:16:00 +0100

    Update 11.Make_whole_Non_pruned.sh

--- VCF_Filtering_and_Renaming/11.Make_whole_Non_pruned.sh
+++ VCF_Filtering_and_Renaming/11.Make_whole_Non_pruned.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 #SBATCH -p cpu-dedicated
 #SBATCH --account=dedicated-cpu@dgimi-eha
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=50G
-#SBATCH --job-name=vcf_filter_biallelic
-#SBATCH --output=logs/vcf_filter_%j.out
-#SBATCH --error=logs/vcf_filter_%j.err
+
 
 # --- Working Directory ---
-cd /storage/simple/projects/faw_adaptation/Data_Backup/Merged_vcf/2025_GenFAW600/whole_genome
+cd /storage/simple/users/durandk/scratch_durandk/GenFAW600/VCF/VCF202510_new/whole_genome/
 
 # --- Environment Setup ---
 source /home/durandk/miniconda3/etc/profile.d/conda.sh
 conda activate vcftools
 
 # --- Input / Output Paths ---
-VCF_IN="GenFAW600_Whole_nocontaminated.vcf.gz.recode.vcf.gz"
-OUTPREFIX="GenFAW600_Whole__biallelic_geno08"
+VCF_IN="/storage/simple/projects/faw_adaptation/Data_Backup/Merged_vcf/2025_GenFAW600/whole_genome/GenFAW600_Whole_nocontaminated.recode.vcf.gz"
+OUTPREFIX="GenFAW600_Whole_biallelic_geno08"
 
-vcftools --gzvcf "$VCF_IN" \
-  --max-missing 0.2 \
-  --min-alleles 2 \
-  --max-alleles 2 \
-  --recode \
-  --recode-INFO-all \
-  --out "${OUTPREFIX}"
+#vcftools --gzvcf "$VCF_IN" \
+#  --remove remove.txt \
+#  --max-missing 0.8 \
+#  --min-alleles 2 \
+#  --max-alleles 2 \
+#  --recode --recode-INFO-all \
+#  --out "${OUTPREFIX}"
 
+# Ensure file is fully written
+sync
 
+# --- Compress + Index ---
 conda activate bgzip_tabix
 
-bgzip -c "${OUTPREFIX}.recode.vcf" > "${OUTPREFIX}.vcf.gz"
-tabix -p vcf "${OUTPREFIX}.vcf.gz"
-
+bgzip -c "${OUTPREFIX}.recode.vcf" > "${OUTPREFIX}.recode2.vcf.gz"
+tabix -p vcf "${OUTPREFIX}.recode2.vcf.gz"
