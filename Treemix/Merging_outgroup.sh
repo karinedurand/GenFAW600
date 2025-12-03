81ce6cbb30617b9aae2ce660a1273f1b7b0a9c09
karine Durand <45596232+karinedurand@users.noreply.github.com>
2025-12-03 15:43:25 +0100

    Refactor Merging_outgroup.sh for BCF merging process

--- Treemix/Merging_outgroup.sh
+++ Treemix/Merging_outgroup.sh
@@ -4,34 +4,34 @@
 #SBATCH --mem=40G
 #SBATCH --cpus-per-task=4
 
+
 source /home/durandk/miniconda3/etc/profile.d/conda.sh
 conda activate bcftools
 
-cd /storage/simple/projects/faw_adaptation/Data_Backup/Merged_vcf/2025_outgroup
+LIST="/home/durandk/scratch_durandk/all_bcf/liste_bcf_amerged.txt"   # ta liste d’échantillons
+DIR="/home/durandk/scratch_durandk/all_bcf"   # répertoire des liens BCF
+TMP="bcf_list_to_merge.txt"
 
-bcftools annotate --rename-chrs /storage/simple/users/durandk/scratch_durandk/GenFAW600/Treemix/rename.txt -O z -o SRR5132437.filtered.renamed.vcf.gz SRR5132437.filtered.vcf.gz
-bcftools view \
-    -r 1 -r 2 -r 3 -r 4 -r 5 -r 6 -r 7 -r 8 -r 9 -r 10 \
-    -r 11 -r 12 -r 13 -r 14 -r 15 -r 16 -r 17 -r 18 -r 19 -r 20 \
-    -r 21 -r 22 -r 23 -r 24 -r 25 -r 26 -r 27 -r 28 -r 29 \
-    SRR5132437.filtered.vcf.gz \
-    -Oz -o SRR5132437.filtered_chr1-29.vcf.gz
-    
-conda activate bgzip_tabix
-tabix -p vcf SRR5132437.filtered_chr1-29.vcf.gz
+# 3. Merge BCF 
 
-cd /storage/simple/projects/faw_adaptation/Data_Backup/Merged_vcf/2025_GenFAW600/whole_genome/without_lab_strain
+bcftools merge \
+     --file-list "$LIST" \
+     -Oz \
+     -o merged.snps.vcf.gz \
+     --threads 4
 
-tabix -p vcf /storage/simple/projects/faw_adaptation/Data_Backup/Merged_vcf/2025_GenFAW600/whole_genome/without_lab_strain/GenFAW600_Whole_biallelic_geno08_Pruned0.5_noASW7_v42.vcf.gz 
+ bcftools view \
+     -v snps \
+     -i 'AC>0' \
+     merged.snps.vcf.gz \
+     -Oz -o merged.snps.filtered_bis.vcf.gz \
+      --threads 4
 
+bcftools index merged.snps.filtered.vcf.gz
 
-conda activate bcftools
+bcftools annotate --rename-chrs rename.txt merged.snps.filtered.vcf.gz \
+  -Oz -o merged.snps.filtered.renamed.vcf.gz --threads 4
 
-bcftools merge GenFAW600_Whole_biallelic_geno08_Pruned0.5_noASW7_v42.vcf.gz   /storage/simple/projects/faw_adaptation/Data_Backup/Merged_vcf/2025_outgroup/SRR5132437.filtered_chr1-29.vcf.gz \
--Oz -o /storage/simple/users/durandk/scratch_durandk/GenFAW600/Treemix/treemix.vcf.gz --threads 4
-
-
-
-
+bcftools view -t 1..29 merged.snps.filtered.renamed.vcf.gz -Oz -o merged.snps.filtered.renamed_chr1-29.vcf.gz --threads 4
 
 
