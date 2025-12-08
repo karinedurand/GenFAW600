06abcb8cf2f9988a7220b3f7b07fa549221c5004
karine Durand <45596232+karinedurand@users.noreply.github.com>
2025-12-08 08:46:29 +0100

    Add files via upload

---
+++ New_vcf/11.Make_whole_Non_pruned.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH -p cpu-dedicated
+#SBATCH --account=dedicated-cpu@dgimi-eha
+#SBATCH --mem=50G
+#SBATCH --job-name=vcf_filter_biallelic
+
+
+# --- Working Directory ---
+cd /storage/simple/projects/faw_adaptation/Data_Backup/Merged_vcf/2025_GenFAW600/whole_genome
+
+# --- Environment Setup ---
+source /home/durandk/miniconda3/etc/profile.d/conda.sh
+conda activate vcftools
+
+# --- Input / Output Paths ---
+VCF_IN="GenFAW600_Whole_nocontaminated.vcf.gz.recode.vcf.gz"
+OUTPREFIX="GenFAW600_Whole_biallelic_geno08"
+bcftools view -t 1..29 "$VCF_IN" -Oz -o "$VCF_IN"_chr1-29
+
+vcftools --gzvcf "$VCF_IN"_chr1-29 \
+  --max-missing 0.2 \
+  --min-alleles 2 \
+  --max-alleles 2 \
+  --recode \
+  --recode-INFO-all \
+  --out "${OUTPREFIX}"
+
+
+conda activate bgzip_tabix
+
+bgzip -c "${OUTPREFIX}.recode.vcf" > "${OUTPREFIX}.vcf.gz"
+tabix -p vcf "${OUTPREFIX}.vcf.gz"
+
